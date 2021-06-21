5e12e8a1185955c7affe694e1f13d0e7c810d034
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-21 06:14:57 +0100

    headers quandrado

---
+++ custom_painter/lib/src/widgets/headers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuandrado extends StatelessWidget {
+  const HeaderCuandrado({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615AAB),
+    );
+  }
+}
