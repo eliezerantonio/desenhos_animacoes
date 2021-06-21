0ca881d386d5648b141982b9d34985e98676b050
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-21 06:35:34 +0100

    hh

--- custom_painter/lib/src/pages/headers_page.dart
+++ custom_painter/lib/src/pages/headers_page.dart
@@ -8,7 +8,7 @@ class HeadersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: HeaderBordersRedondeados(),
+      child: HeaderDiagonal(),
     ));
   }
 }
--- custom_painter/lib/src/widgets/headers.dart
+++ custom_painter/lib/src/widgets/headers.dart
@@ -18,10 +18,22 @@ class HeaderBordersRedondeados extends StatelessWidget {
     return Container(
       height: 300,
       decoration: BoxDecoration(
-          color: Color(0xff615AAB),
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(70),
-              bottomRight: Radius.circular(50))),
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
     );
+  }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 300,
+        color: Color(0xff615AAB),
+      );
   }
 }
