651c91030b01aabc94e19e43203dcb927c75e568
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-21 06:57:44 +0100

    painter diagonal

--- custom_painter/lib/src/widgets/headers.dart
+++ custom_painter/lib/src/widgets/headers.dart
@@ -32,8 +32,38 @@ class HeaderDiagonal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
-        color: Color(0xff615AAB),
-      );
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    //properties
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill; //fill
+    paint.strokeWidth = 20;
+
+//partes onde quero mover o paint
+    final path = Path();
+
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    // path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
