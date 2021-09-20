0ec7db5ea3f53937b92d233034629cb56316f347
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-20 02:33:27 +0100

    disco

--- musicplayer/lib/src/pages/music_player_page.dart
+++ musicplayer/lib/src/pages/music_player_page.dart
@@ -8,9 +8,74 @@ class MusicaPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        children: [CustomAppBar(), ImageDiskAndDuration()],
+      ),
+    );
+  }
+}
+
+class ImageDiskAndDuration extends StatelessWidget {
+  const ImageDiskAndDuration({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
         children: [
-          CustomAppBar(),
+          DiskImage()
+          //TODO:BArRA PROGRESSO
         ],
+      ),
+    );
+  }
+}
+
+class DiskImage extends StatelessWidget {
+  const DiskImage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(200),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image(
+                image: AssetImage("assets/album.jpg"),
+              ),
+              Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              Container(
+                width: 18,
+                height: 18,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ],
+          )),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Colors.grey[200],
+            Colors.grey[50],
+          ],
+        ),
       ),
     );
   }
