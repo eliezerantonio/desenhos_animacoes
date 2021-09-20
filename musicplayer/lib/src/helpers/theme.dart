e630826bddbe21818df78e089832755399c9fd25
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-20 01:36:13 +0100

    music player

---
+++ musicplayer/lib/src/helpers/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+final miTema = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: Color(0xff201E28),
+  // scaffoldBackgroundColor: Color(0xff40404C),
+  iconTheme: IconThemeData(
+    color: Colors.white.withOpacity(0.3)
+  )
+);
