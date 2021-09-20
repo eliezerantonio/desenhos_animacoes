325518ec487ac7b7555f1eddc5a90ecf33a7cd4b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-20 04:15:05 +0100

    letras

--- musicplayer/lib/src/helpers/theme.dart
+++ musicplayer/lib/src/helpers/theme.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 final miTema = ThemeData.light().copyWith(
-    scaffoldBackgroundColor: Colors.white,
-    // scaffoldBackgroundColor: Color(0xff40404C),
-    iconTheme: IconThemeData(color: Colors.grey[400]));
+  scaffoldBackgroundColor: Colors.white,
+  // scaffoldBackgroundColor: Color(0xff40404C),
+  iconTheme: IconThemeData(
+    color: Colors.grey[400],
+  ),
+  textTheme: TextTheme(
+    headline6: TextStyle(
+      color: Colors.grey[400],
+    ),
+  ),
+);
--- musicplayer/lib/src/pages/music_player_page.dart
+++ musicplayer/lib/src/pages/music_player_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:musicplayer/src/helpers/helpers.dart';
 import 'package:musicplayer/src/widgets/custom_app_bar.dart';
 
 class MusicaPlayerPage extends StatelessWidget {
@@ -11,6 +13,8 @@ class MusicaPlayerPage extends StatelessWidget {
         children: [
           CustomAppBar(),
           ImageDiskAndDuration(),
+          PlayTitle(),
+          Expanded(child: Lyrics())
         ],
       ),
     );
@@ -109,7 +113,7 @@ class ProgressBar extends StatelessWidget {
                 child: Container(
                   width: 3,
                   height: 150,
-                  color: Colors.grey[400].withOpacity(0.8),
+                  color: Color(0xffFE0240),
                 ),
               ),
             ],
@@ -127,6 +131,115 @@ class ProgressBar extends StatelessWidget {
   TextStyle buildTextStyle() {
     return TextStyle(
       color: Colors.grey.withOpacity(0.8),
+    );
+  }
+}
+
+class PlayTitle extends StatelessWidget {
+  const PlayTitle({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text(
+            "Save Your Tears",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.grey.withOpacity(0.9),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "-The Weeknd-",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.grey.withOpacity(0.6),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ControllButtons(),
+        ],
+      ),
+    );
+  }
+}
+
+class Lyrics extends StatelessWidget {
+  const Lyrics({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final lyrics = getLyrics();
+    return Container(
+        child: ListWheelScrollView(
+      itemExtent: 42,
+      diameterRatio: 1.5,
+      physics: BouncingScrollPhysics(),
+      children: lyrics
+          .map((line) => Text(
+                line,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey.withOpacity(0.9),
+                ),
+              ))
+          .toList(),
+    ));
+  }
+}
+
+class ControllButtons extends StatelessWidget {
+  const ControllButtons({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: 50,
+          margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.22),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(
+                color: Colors.grey[400],
+              )),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.3),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(FontAwesomeIcons.angleDoubleLeft, size: 30),
+              Container(
+                height: 120,
+                width: 90,
+                child: FloatingActionButton(
+                  backgroundColor: Color(0xffFE0240),
+                  onPressed: () {},
+                  child: Center(
+                    child: Icon(FontAwesomeIcons.play,
+                        color: Colors.white, size: 32),
+                  ),
+                ),
+              ),
+              Icon(FontAwesomeIcons.angleDoubleRight, size: 30),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
