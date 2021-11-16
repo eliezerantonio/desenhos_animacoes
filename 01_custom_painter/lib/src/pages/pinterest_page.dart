36ff9a37f2f655fd5c0a37afd2ff92218468d563
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-16 21:11:33 +0100

    pinterestmenu

---
+++ 01_custom_painter/lib/src/pages/pinterest_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class PinterestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: PinterestGrid(),
+      ),
+    );
+  }
+}
+
+class PinterestGrid extends StatelessWidget {
+  final List<int> items = List.generate(200, (index) => index);
+  @override
+  Widget build(BuildContext context) {
+    return new StaggeredGridView.countBuilder(
+      crossAxisCount: 4,
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) =>
+          _PinterestItem(index: index),
+      staggeredTileBuilder: (int index) =>
+          new StaggeredTile.count(2, index.isEven ? 2 : 3),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  }
+}
+
+class _PinterestItem extends StatelessWidget {
+  final int index;
+
+  const _PinterestItem({Key key, this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: new Center(
+          child: new CircleAvatar(
+            backgroundColor: Colors.white,
+            child: new Text('$index'),
+          ),
+        ));
+  }
+}
