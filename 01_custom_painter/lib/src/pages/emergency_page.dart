34b39c84f6ac6d3a1616b1e6edcd5e2d3546f879
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-19 00:27:00 +0100

    desnho do header

--- 01_custom_painter/lib/src/pages/emergency_page.dart
+++ 01_custom_painter/lib/src/pages/emergency_page.dart
@@ -1,14 +1,11 @@
+import 'package:custom_painter/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 
-
 class EmergencyPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+      body: IconHeader(),
+    );
   }
 }
--- 01_custom_painter/lib/src/widgets/headers.dart
+++ 01_custom_painter/lib/src/widgets/headers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HeaderCuandrado extends StatelessWidget {
   const HeaderCuandrado({Key key}) : super(key: key);
@@ -203,5 +204,81 @@ class _HeaderWaveGradiat extends CustomPainter {
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
+  }
+}
+
+class IconHeader extends StatelessWidget {
+  final Color whiteColor = Colors.white.withOpacity(0.7);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 250,
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        Column(
+          children: [
+            SizedBox(
+              height: 80,
+              width: double.infinity,
+            ),
+            Text(
+              'Solicite',
+              style: TextStyle(
+                fontSize: 20,
+                color: whiteColor,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Assistencia Medica',
+              style: TextStyle(
+                  fontSize: 25, color: whiteColor, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FaIcon(FontAwesomeIcons.plus, size: 80, color: Colors.white)
+          ],
+        )
+      ],
+    );
+  }
+}
+//hey Copilot!, how are you?
+
+class _IconHeaderBackground extends StatelessWidget {
+  const _IconHeaderBackground({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(80),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xff526bf6),
+            Color(0xff67acf2),
+          ],
+        ),
+      ),
+    );
   }
 }
