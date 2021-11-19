91b92d811f213498a53350dafc94258aecd26b55
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-19 01:44:05 +0100

    mostando todos butoes

--- 01_custom_painter/lib/src/pages/emergency_page.dart
+++ 01_custom_painter/lib/src/pages/emergency_page.dart
@@ -4,18 +4,66 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EmergencyPage extends StatelessWidget {
+  final items = <ItemBoton>[
+    new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+        Color(0xff6989F5), Color(0xff906EF5)),
+    new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2),
+        Color(0xff536CF6)),
+    new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+        Color(0xffF2D572), Color(0xffE06AA3)),
+    new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+        Color(0xff46997D)),
+    new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+        Color(0xff6989F5), Color(0xff906EF5)),
+    new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2),
+        Color(0xff536CF6)),
+    new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+        Color(0xffF2D572), Color(0xffE06AA3)),
+    new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+        Color(0xff46997D)),
+    new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+        Color(0xff6989F5), Color(0xff906EF5)),
+    new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2),
+        Color(0xff536CF6)),
+    new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+        Color(0xffF2D572), Color(0xffE06AA3)),
+    new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+        Color(0xff46997D)),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Builder(builder: (context) {
-          return FatButton(
+    List<Widget> ItemMap = items
+        .map((item) => FatButton(
+            title: item.texto,
+            color1: item.color1,
+            color2: item.color2,
             onPressd: () {},
-            title: 'Motor acident',
-            color1: Color(0xff6989f5),
-            color2: Color(0xff906ef5),
-          );
-        }),
+            icon: item.icon))
+        .toList();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 200),
+            child: ListView(
+              physics: BouncingScrollPhysics(),
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                ...ItemMap
+              ],
+            ),
+          ),
+          IconHeader(
+            icon: FontAwesomeIcons.plus,
+            title: "Assitencia medica",
+            subtitle: "Solicite",
+            color1: Color(0xff536CF6),
+            color2: Color(0xff66A9F2),
+          )
+        ],
       ),
     );
   }
@@ -36,4 +84,13 @@ class PageHeader extends StatelessWidget {
       color2: Color(0xff67acf2),
     );
   }
+}
+
+class ItemBoton {
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton(this.icon, this.texto, this.color1, this.color2);
 }
