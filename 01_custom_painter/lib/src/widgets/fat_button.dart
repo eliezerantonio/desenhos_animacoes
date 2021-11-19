1207bce44f1271835cc99f1ddddd00cbe40fb25e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-19 01:16:55 +0100

    botao gordo

--- 01_custom_painter/lib/src/widgets/fat_button.dart
+++ 01_custom_painter/lib/src/widgets/fat_button.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FatButton extends StatelessWidget {
   const FatButton({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return _FatButtonBackground();
+    return Stack(
+      children: [
+        _FatButtonBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 140,
+              width: 40,
+            ),
+            FaIcon(FontAwesomeIcons.carCrash, color: Colors.white, size: 40),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Text(
+                "Motor Accident",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            FaIcon(FontAwesomeIcons.chevronRight,
+                color: Colors.white, size: 20),
+            SizedBox(
+              width: 40,
+            )
+          ],
+        )
+      ],
+    );
   }
 }
 
@@ -17,6 +49,22 @@ class _FatButtonBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            Positioned(
+              top: -20,
+              right: -20,
+              child: FaIcon(
+                FontAwesomeIcons.carCrash,
+                color: Colors.white.withOpacity(0.2),
+                size: 150,
+              ),
+            )
+          ],
+        ),
+      ),
       margin: EdgeInsets.all(20),
       width: double.infinity,
       height: 100,
